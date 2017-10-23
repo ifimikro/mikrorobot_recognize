32898549a86f2b54bfd052aaf0bde36fa68baffa
Vetle Bu Solgård <vetle.busol@outlook.com>
2017-10-23 18:34:48 +0200

    structured code and added some functions. Missing: find faces from saved json file

--- face_µ.py
+++ face_µ.py
@@ -1,76 +1,102 @@
-import face_recognition
+import face_recognition as frec
 import cv2
 import numpy as np
-import pyttsx3
-
-'''
-    Notes:
-        - add server to get images from
-        - work on robustness and speed
-        - have a faster algorithm finding people, use this to remember faces
-'''
+import json
 
 # Reference to webcam
 video_capture = cv2.VideoCapture(0)
 
-# load sample and learn to recognize
-obama_image = face_recognition.load_image_file("myself2.jpg")
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+class Face_Recog:
+    def __init__(self, gtk_mode):
+        self.gtk_mode = gtk_mode
+        self.known_faces = self.load_known_faces()
 
-trump_image = face_recognition.load_image_file("trump.jpg")
-trump_face_encoding = face_recognition.face_encodings(trump_image)[0]
+        # Starting main loop
+        self.face_rec_loop()
 
-face_locations = []
-face_encodings = []
-face_names = []
-process_this_frame = True
+        self.close_release_quit()
 
-# webcam readloop
-while True:
-    # Grab a single frame of video
-    ret, frame = video_capture.read()
-    small_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
-
-    if process_this_frame:
-        face_locations = face_recognition.face_locations(small_frame)
-        face_encodings = face_recognition.face_encodings(small_frame, face_locations)
-
+    def face_rec_loop(self):
+        face_locations = []
+        face_encodings = []
         face_names = []
-        for face_encoding in face_encodings:
-            # See if the face is a match for the known face(s)
-            match = face_recognition.compare_faces([obama_face_encoding, trump_face_encoding], face_encoding)
-            name = "Unknown"
+        face_buffer = []
+        face_i = 0
+        process_this_frame = True
+        while True:
+            ret, frame = video_capture.read()
+            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-            if match[0]:
-                name = "Vetle"
-            elif match[1]:
-                name = "trump"
+            if process_this_frame:
+                face_locations = frec.face_locations(small_frame)
+                face_encodings = frec.face_encodings(small_frame, face_locations)
+                self.find_faces(face_locations, frame)
+                if get_to_know_mode:
+                    if len(face_buffer) == 0 and np.shape(face_encodings)[0] == 1:
+                        face_buffer.append(face_encodings[0])
+                    if len(face_buffer) > 0 and len(face_encodings) == 1:
+                        if frec.compare_faces([face_buffer[face_i]], face_encodings[0]):
+                            face_buffer.append(face_encodings[0])
+                            face_i += 1
+                    if len(face_buffer) == 5:
+                        add_face(face_buffer)
+                        del face_buffer[:]
+                        face_i = 0
 
+            # Show frame
+            cv2.imshow('Video', frame)
+            # Limit processing
+            process_this_frame = not process_this_frame
+            # To exit readloop
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                return
 
-            face_names.append(name)
-    process_this_frame = not process_this_frame
+    def load_known_faces(self):
+        with open('faces.json') as json_data:
+            return json.load(json_data)
 
-    for (top, right, bottom, left), name in zip(face_locations, face_names):
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 2
-        right *= 2
-        bottom *= 2
-        left *= 2
+    def save_face_to_file(self, json_face):
+        with open("faces.json", "w") as outfile:
+            json.dump(json_face, outfile, indent=4)
 
-        # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+    def add_face(self, faces):
+        new_face = (faces[0] + faces[1] + faces[2] + faces[3] + faces[4]) / 5
+        cv2.imshow('face', new_face)
+        name = input('What is the name of the person?\n-> ')
+        save_face_to_file({'name': name,'encoding': new_face.tolist()})
 
-        # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+    def find_faces(self, face_locations, frame):
+        for (top, right, bottom, left) in face_locations:
+            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            #font = cv2.FONT_HERSHEY_DUPLEX
+            #cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-    # Display the resulting image
-    cv2.imshow('Video', frame)
+    def close_release_quit(self):
+        video_capture.release()
+        cv2.destroyAllWindows()
 
-    # To exit readloop
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-video_capture.release()
-cv2.destroyAllWindows()
+    '''
+    def find_known_faces(self, face_locations, frame):
+        for (top, right, bottom, left) in face_locations:
+            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+    '''
+GTK_MODE = True
+Face_Recog(GTK_MODE)
